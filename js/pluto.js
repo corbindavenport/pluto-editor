0b799d8a0447cbd2b06d42f06d4b79b8362475ec
Corbin Davenport <me@corbin.io>
2021-12-24 16:10:17 -0500

    Add header and clipboard options

--- js/pluto.js
+++ js/pluto.js
@@ -1,16 +1,17 @@
 function getSelectedText() {
     // Get text selection
     var textEl = window.getSelection()
-    console.log(textEl)
-    if (textEl.type != 'Range') {
-        console.log('No text selected')
+    console.log('Text selection:', textEl)
+    // Ensure only the editor area can be modified
+    if (!document.getElementById('pluto-editor').contains(textEl.focusNode)) {
+        console.error('Selection area is outside editor.')
         return null
     }
     var selectedString = textEl.focusNode.data.slice(textEl.anchorOffset, textEl.focusOffset)
     // Return data
     var data = {
         'node': textEl,
-        'parent': textEl.focusNode.parentNode,
+        'parentNode': textEl.focusNode.parentNode,
         'selected': selectedString
     }
     return data
@@ -72,6 +73,17 @@ document.querySelector('#open-file').addEventListener('click', async function ()
     }
 })
 
+document.querySelector('#open-clipboard').addEventListener('click', async function () {
+    // Read from clipboard
+    var clipText = await navigator.clipboard.readText()
+    // Switch editor to new file
+    var converter = new showdown.Converter()
+    var html = converter.makeHtml(clipText)
+    document.getElementById('pluto-editor').innerHTML = html
+    updateFileName('clipboard.md', Boolean(false))
+    updateWordCount()
+})
+
 document.querySelector('#save-file').addEventListener('click', async function () {
     // Generate Markdown
     var converter = new showdown.Converter()
@@ -85,7 +97,9 @@ document.querySelector('#save-file').addEventListener('click', async function ()
         updateFileName(globalFileName)
     } else {
         // Save file without File Access API
-        alert('Your browser does not support the File Access API, so your changes will be saved as a new file in your Downloads folder.')
+        if (!('showDirectoryPicker' in window)) {
+            alert('Your browser does not support the File Access API, so your changes will be saved as a new file in your Downloads folder.')
+        }
         saveAs(blob, globalFileName)
         updateFileName(globalFileName)
     }
@@ -116,6 +130,18 @@ document.querySelector('#save-file-as').addEventListener('click', async function
     await writable.close()
 })
 
+document.querySelector('#save-clipboard').addEventListener('click', async function () {
+    // Convert to Markdown
+    var converter = new showdown.Converter()
+    var output = converter.makeMarkdown(document.getElementById('pluto-editor').innerHTML)
+    // Write to clipboard
+    navigator.clipboard.writeText(output).then(function () {
+        alert('The Markdown version has been copied to your clipboard!')
+    }, function () {
+        alert('There was an error, sorry!')
+    })
+})
+
 /* Editor buttons */
 
 document.querySelector('#pluto-editor-bold').addEventListener('click', function () {
@@ -124,6 +150,35 @@ document.querySelector('#pluto-editor-bold').addEventListener('click', function 
 
 document.querySelector('#pluto-editor-italic').addEventListener('click', function () {
     document.execCommand('italic')
+})
+
+document.querySelector('#pluto-editor-strike').addEventListener('click', function () {
+    document.execCommand('strikeThrough')
+})
+
+document.querySelectorAll('.pluto-editor-header-btn').forEach(function (el) {
+    el.addEventListener('click', function () {
+        var tag = 'h' + el.dataset.size
+        var text = getSelectedText()
+        // Create header element
+        var headEl = document.createElement(tag)
+        headEl.innerText = text.parentNode.innerText
+        text.parentNode.parentNode.replaceChild(headEl, text.parentNode)
+    })
+})
+
+document.querySelector('#pluto-editor-paragraph-btn').addEventListener('click', function () {
+    var text = getSelectedText()
+    // Create paragraph element
+    var pEl = document.createElement('p')
+    pEl.innerText = text.parentNode.innerText
+    text.parentNode.parentNode.replaceChild(pEl, text.parentNode)
+})
+
+document.querySelectorAll('button, .dropdown-item').forEach(function (el) {
+    el.addEventListener('mousedown', function (e) {
+        e.preventDefault()
+    }, false)
 })
 
 document.querySelector('#pluto-editor-about').addEventListener('click', function () {
@@ -149,33 +204,33 @@ document.querySelector('#import').addEventListener('change', function (el) {
 /* Word count updating */
 
 function updateWordCount() {
-        // Update word count
-        var wordCount = document.getElementById('pluto-editor').textContent.trim().split(/\s+/).length
-        document.getElementById('pluto-word-count').innerText = wordCount
-        // Update character count
-        var wordCount = document.getElementById('pluto-editor').textContent.trim().length
-        document.getElementById('pluto-char-count').innerText = wordCount
-    }
+    // Update word count
+    var wordCount = document.getElementById('pluto-editor').textContent.trim().split(/\s+/).length
+    document.getElementById('pluto-word-count').innerText = wordCount
+    // Update character count
+    var wordCount = document.getElementById('pluto-editor').textContent.trim().length
+    document.getElementById('pluto-char-count').innerText = wordCount
+}
 
 document.getElementById('pluto-editor').addEventListener('DOMCharacterDataModified', function () {
-        updateWordCount()
-        updateFileName(globalFileName, new Boolean(true))
-    })
+    updateWordCount()
+    updateFileName(globalFileName, new Boolean(true))
+})
 
 updateWordCount()
 
 /* File name updating */
 
 function updateFileName(newglobalFileName, isModified = Boolean(false)) {
-        if (newglobalFileName != globalFileName) {
-            globalFileName = newglobalFileName
-            document.title = newglobalFileName
-            document.getElementById('pluto-file-name').innerText = newglobalFileName
-        } else if (isModified) {
-            document.title = globalFileName + '*'
-            document.getElementById('pluto-file-name').innerText = newglobalFileName + '*'
-        } else {
-            document.title = globalFileName
-            document.getElementById('pluto-file-name').innerText = newglobalFileName
-        }
+    if (newglobalFileName != globalFileName) {
+        globalFileName = newglobalFileName
+        document.title = newglobalFileName
+        document.getElementById('pluto-file-name').innerText = newglobalFileName
+    } else if (isModified) {
+        document.title = globalFileName + '*'
+        document.getElementById('pluto-file-name').innerText = newglobalFileName + '*'
+    } else {
+        document.title = globalFileName
+        document.getElementById('pluto-file-name').innerText = newglobalFileName
     }
+}
